e528955b14d5b8bd6eafd5254b22c285c06eee36
Daniel Jones <danieljones@Daniels-MBP-3.rochester.rr.com>
2020-02-20 14:23:45 -0500

    Adding '/remoteExecute' endpoint for remote CSM

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require('body-parser')
 const { PORT, EXPRESS_PORT } = require("./constants");
 const isDev = require("electron-is-dev");
 const path = require("path");
+const { executeCommunication } = require('@ge-fnm/csm')
 
 const app = express();
 
 app.use(cors());
+// middleware to automatically parse request bodies to JSON
+app.use(bodyParser.json())
 
 if (isDev) {
   console.log(
@@ -37,6 +41,15 @@ app.get("/serialInfo", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+app.post("/remoteExecute",  (req, res) => {
+  // get the serialized action object string from the post request body
+  const serializedActionObject = req.body.serializedAction
+  // pass string along to CSM, let CSM handle success/error logic
+  executeCommunication(serializedActionObject)
+    .then(data => res.json({data: data}))
+    .catch(err => res.status(500).json(err))
+})
 
 app.listen(PORT, () =>
   console.log(`Express server listening on port ${PORT}!`)
