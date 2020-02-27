2d0458192a772b57754db3bbda542c39d7a58a26
Daniel Jones <danielkjones0@gmail.com>
2020-02-27 13:22:40 -0500

    Frontend Build Fix (#3)
    * fix: removing preflight check for building CRA
    
    * Adding back necessary lines to clone script
    
    * Moving frontend clone/build to /tmp directory
    
    * Adding ability to checkout different frontend versions

--- scripts/cloneFrontend.js
+++ scripts/cloneFrontend.js
@@ -1,16 +1,21 @@
 const {execSync} = require("child_process")
 const shell = require("shelljs")
+require("dotenv").config()
 
 const frontendUrl = "https://github.com/GE-MDS-FNM-V2/frontend.git"
+const frontendBranch = process.env.FRONTEND_DEPLOYMENT_BRANCH
 
-shell.rm("-rf", "build")
-shell.mkdir("build")
-shell.cd("build")
-shell.exec(`git clone ${frontendUrl}`)
+const projectDirectory = process.cwd()
+const frontendDirectoryName = "/tmp/.ge-fnm-react-app"
+
+shell.rm("-rf", frontendDirectoryName)
+shell.mkdir(frontendDirectoryName)
+shell.cd(frontendDirectoryName)
+shell.exec(`git clone -b ${frontendBranch} ${frontendUrl}`)
 shell.cd("frontend")
 shell.exec("yarn")
 shell.exec("yarn build")
-shell.cp("-r", "./build", "../../src/compiled-frontend")
-shell.cd("../..")
-shell.rm("-rf", "build")
+shell.cp("-r", "./build", `${projectDirectory}/src/compiled-frontend`)
+shell.cd(projectDirectory)
+shell.rm("-rf", frontendDirectoryName)
 // execSync(`rm -rf build && mkdir build && cd build && git clone ${frontendUrl} && cd frontend && yarn && yarn build && cp -r ./build ../../src/compiled-frontend && cd ../.. && rm -rf build`)
