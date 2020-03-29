ed2f11a68f0040eea18fe3b4ab1027b87c03f96b
Brady Brown <bobrown101@gmail.com>
2020-03-29 14:32:52 -0400

    changed frontendUrl

--- scripts/cloneFrontend.js
+++ scripts/cloneFrontend.js
@@ -2,7 +2,7 @@ const {execSync} = require("child_process")
 const shell = require("shelljs")
 require("dotenv").config()
 
-const frontendUrl = "https://github.com/GE-MDS-FNM-V2/frontend.git"
+const frontendUrl = "https://github.com/GE-MDS-FNM-V2/react-ui.git"
 const frontendBranch = process.env.FRONTEND_DEPLOYMENT_BRANCH
 
 const projectDirectory = process.cwd()
@@ -19,4 +19,3 @@ shell.exec("yarn build")
 shell.cp("-r", "./build", `${projectDirectory}/src/compiled-frontend`)
 shell.cd(projectDirectory)
 shell.rm("-rf", frontendDirectoryName)
-// execSync(`rm -rf build && mkdir build && cd build && git clone ${frontendUrl} && cd frontend && yarn && yarn build && cp -r ./build ../../src/compiled-frontend && cd ../.. && rm -rf build`)
